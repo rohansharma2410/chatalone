ac37961bf904b674246567416711dee9af102ad0
Rohan sharma <sharmar2410@gmail.com>
2023-09-19 18:09:46 +0530

    1.5

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,214 +1,209 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component, useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { auth } from "../services/firebase";
 import { db } from "../services/firebase";
 
-export default class Chat extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: auth().currentUser,
-      friendName: null,
-      chats: [],
-      content: "",
-      deletePrompt: false,
-      deletionMsgRef: "",
-      readError: null,
-      writeError: null,
-      loadingChats: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.myRef = React.createRef();
-  }
+export default function Chat() {
+  const { chatID } = useParams();
+  const navigate = useNavigate();
 
-  async componentDidMount() {
-    this.setState({ readError: null, loadingChats: true });
-    const chatArea = this.myRef.current;
-    try {
-      const chatid = this.props.match.params.chatID;
-      if (!chatid.split("_").includes(this.state.user.uid)) {
-        this.props.history.push("/chat");
-        throw new Error("You shouldn't be here 🤨");
-      }
+  const [user, setUser] = useState(auth().currentUser);
+  const [friendName, setFriendName] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [content, setContent] = useState("");
+  const [deletePrompt, setDeletePrompt] = useState(false);
+  const [deletionMsgRef, setDeletionMsgRef] = useState("");
+  const [readError, setReadError] = useState(null);
+  const [writeError, setWriteError] = useState(null);
+  const [loadingChats, setLoadingChats] = useState(false);
 
-      await this.setFriendName();
-      const chatRef = db.ref(`chats/${chatid}`);
-      chatRef.on("value", (snapshot) => {
-        let chats = [];
-        snapshot.forEach((snap) => {
-          chats.push(snap.val());
-        });
-        chats.sort(function (a, b) {
-          return a.timestamp - b.timestamp;
-        });
-        this.setState({ chats });
-        this.setState({ loadingChats: false });
-        chatArea.scrollBy(0, chatArea.scrollHeight);
-      });
-    } catch (error) {
-      this.setState({ readError: error.message, loadingChats: false });
-    }
-  }
+  const myRef = React.createRef();
 
-  handleChange(event) {
-    this.setState({
-      content: event.target.value,
-    });
-  }
+  useEffect(() => {
+    setLoadingChats(true);
+    const chatArea = myRef.current;
 
-  async handleSubmit(event) {
-    event.preventDefault();
-    this.setState({ writeError: null });
-    const chatArea = this.myRef.current;
-    if (this.state.content) {
+    const fetchData = async () => {
       try {
-        const chatid = this.props.match.params.chatID;
-        if (!chatid.split("_").includes(this.state.user.uid)) {
-          this.props.history.push("/chat");
+        if (!chatID.split("_").includes(user.uid)) {
+          navigate("/chat");
           throw new Error("You shouldn't be here 🤨");
         }
-        const chatRef = db.ref(`chats/${chatid}`);
-        const newMessageRef = chatRef.push();
-        await newMessageRef.set({
-          content: this.state.content,
-          timestamp: Date.now(),
-          uid: this.state.user.uid,
-          uname: this.state.user.displayName,
+
+        const chatRef = db.ref(`chats/${chatID}`);
+        chatRef.on("value", (snapshot) => {
+          let chats = [];
+          snapshot.forEach((snap) => {
+            chats.push(snap.val());
+          });
+          chats.sort(function (a, b) {
+            return a.timestamp - b.timestamp;
+          });
+          setChats(chats);
+          setLoadingChats(false);
+          chatArea.scrollBy(0, chatArea.scrollHeight);
         });
-        this.setState({ content: "" });
+
+        await fetchFriendName();
+      } catch (error) {
+        setReadError(error.message);
+        setLoadingChats(false);
+      }
+    };
+
+    fetchData();
+  }, [chatID, user.uid, navigate]);
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setWriteError(null);
+    const chatArea = myRef.current;
+
+    if (content) {
+      try {
+        if (!chatID.split("_").includes(user.uid)) {
+          navigate("/chat");
+          throw new Error("You shouldn't be here 🤨");
+        }
+
+        const chatRef = db.ref(`chats/${chatID}`);
+        const newMessageRef = chatRef.push();
+
+        await newMessageRef.set({
+          content: content,
+          timestamp: Date.now(),
+          uid: user.uid,
+          uname: user.displayName,
+        });
+
+        setContent("");
         document.querySelector(".chat-input").focus();
         chatArea.scrollBy(0, chatArea.scrollHeight);
       } catch (error) {
-        this.setState({ writeError: error.message });
+        setWriteError(error.message);
       }
     }
-  }
+  };
 
-  async setFriendName() {
-    const chatID = this.props.match.params.chatID;
+  const fetchFriendName = async () => {
     const x = chatID.split("_");
-    const friendID = x[0] === this.state.user.uid ? x[1] : x[0];
+    const friendID = x[0] === user.uid ? x[1] : x[0];
 
-    await db
-      .ref(`users/${friendID}`)
-      .once("value")
-      .then((snapshot) => {
-        this.setState({ friendName: snapshot.val().uname });
-      });
-  }
+    try {
+      const snapshot = await db.ref(`users/${friendID}`).once("value");
+      setFriendName(snapshot.val().uname);
+    } catch (error) {
+      // Handle error if necessary
+    }
+  };
 
-  formatTime(timestamp) {
+  const formatTime = (timestamp) => {
     const d = new Date(timestamp);
     const hours = d.getHours().toString().padStart(2, "0");
     const minutes = d.getMinutes().toString().padStart(2, "0");
     const time = `${hours}:${minutes}`;
     return time;
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        {/* Loading indicator */}
-        {this.state.loadingChats ? <div className="spinner"></div> : ""}
-        <section className="chat-container">
-          <header className="chat-header">
-            <Link to="/chat" className="px-2">
-              <i className="fas fa-chevron-left"></i>
+  return (
+    <div>
+      {/* Loading indicator */}
+      {loadingChats ? <div className="spinner"></div> : ""}
+      <section className="chat-container">
+        <header className="chat-header">
+          <Link to="/chat" className="px-2">
+            <i className="fas fa-chevron-left"></i>
+          </Link>
+          <div className="chat-header-title">{friendName}</div>
+          <div className="chat-settings">
+            <Link to="/" className="px-2">
+              <i className="fas fa-cog"></i>
             </Link>
-            <div className="chat-header-title">{this.state.friendName}</div>
-            <div className="chat-settings">
-              <Link to="/" className="px-2">
-                <i className="fas fa-cog"></i>
-              </Link>
-            </div>
-          </header>
-          {this.state.readError ? (
-            <div className="alert alert-danger py-0 rounded-0" role="alert">
-              {this.state.readError}
-            </div>
-          ) : null}
+          </div>
+        </header>
+        {readError ? (
+          <div className="alert alert-danger py-0 rounded-0" role="alert">
+            {readError}
+          </div>
+        ) : null}
 
-          <main className="chatarea" ref={this.myRef}>
-            {/* Chat area */}
-            {this.state.chats.map((chat) => {
-              return (
+        <main className="chatarea" ref={myRef}>
+          {/* Chat area */}
+          {chats.map((chat) => {
+            return (
+              <div
+                key={chat.timestamp}
+                className={
+                  "msg " + (user.uid === chat.uid ? "right-msg" : "left-msg")
+                }
+              >
                 <div
-                  key={chat.timestamp}
-                  className={
-                    "msg " +
-                    (this.state.user.uid === chat.uid ? "right-msg" : "left-msg")
-                  }
-                >
-                  <div
-                    className="chat-bubble"
-                    onDoubleClick={async () => {
-                      if (chat.uid !== this.state.user.uid) return;
-                      const chatid = this.props.match.params.chatID;
-                      this.setState({ deletePrompt: true });
-                      const x = await db
-                        .ref(`chats/${chatid}`)
-                        .orderByChild("timestamp")
-                        .equalTo(chat.timestamp)
-                        .once("value");
-                      this.setState({
-                        deletionMsgRef: `chats/${chatid}/${Object.keys(
-                          x.val()
-                        )[0]}`,
-                      });
-                    }}
-                  >
-                    <div className="msg-text">{chat.content}</div>
-                    <div className="chat-info-time noselect text-right">
-                      {this.formatTime(chat.timestamp)}
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </main>
-          {this.state.deletePrompt ? (
-            <div
-              className="d-flex justify-content-between align-items-center alert alert-danger mb-0 mt-1 rounded-0 py-1 px-2"
-              role="alert"
-            >
-              <span>Do you wish to delete that message?</span>
-              <div className="d-flex">
-                <button
-                  type="button"
-                  className="btn btn-sm py-0 mr-1 btn-outline-danger"
-                  onClick={() => {
-                    db.ref(this.state.deletionMsgRef).remove();
-                    this.setState({ deletePrompt: false });
+                  className="chat-bubble"
+                  onDoubleClick={async () => {
+                    if (chat.uid !== user.uid) return;
+                    setDeletePrompt(true);
+                    const x = await db
+                      .ref(`chats/${chatID}`)
+                      .orderByChild("timestamp")
+                      .equalTo(chat.timestamp)
+                      .once("value");
+                    setDeletionMsgRef(`chats/${chatID}/${Object.keys(x.val())[0]}`);
                   }}
                 >
-                  Yes
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-sm py-0 btn-outline-success"
-                  onClick={() => this.setState({ deletePrompt: false })}
-                >
-                  No
-                </button>
+                  <div className="msg-text">{chat.content}</div>
+                  <div className="chat-info-time noselect text-right">
+                    {formatTime(chat.timestamp)}
+                  </div>
+                </div>
               </div>
+            );
+          })}
+        </main>
+        {deletePrompt ? (
+          <div
+            className="d-flex justify-content-between align-items-center alert alert-danger mb-0 mt-1 rounded-0 py-1 px-2"
+            role="alert"
+          >
+            <span>Do you wish to delete that message?</span>
+            <div className="d-flex">
+              <button
+                type="button"
+                className="btn btn-sm py-0 mr-1 btn-outline-danger"
+                onClick={() => {
+                  db.ref(deletionMsgRef).remove();
+                  setDeletePrompt(false);
+                }}
+              >
+                Yes
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm py-0 btn-outline-success"
+                onClick={() => setDeletePrompt(false)}
+              >
+                No
+              </button>
             </div>
-          ) : null}
-          <form onSubmit={this.handleSubmit} className="chat-inputarea">
-            <input
-              type="text"
-              placeholder="Message..."
-              className="chat-input"
-              name="content"
-              onChange={this.handleChange}
-              value={this.state.content}
-            ></input>
-            <button type="submit" className="chat-sendbtn">
-              Send
-            </button>
-          </form>
-        </section>
-      </div>
-    );
-  }
+          </div>
+        ) : null}
+        <form onSubmit={handleSubmit} className="chat-inputarea">
+          <input
+            type="text"
+            placeholder="Message..."
+            className="chat-input"
+            name="content"
+            onChange={handleChange}
+            value={content}
+          ></input>
+          <button type="submit" className="chat-sendbtn">
+            Send
+          </button>
+        </form>
+      </section>
+    </div>
+  );
 }
